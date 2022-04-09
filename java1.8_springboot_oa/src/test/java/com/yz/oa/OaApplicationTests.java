39144283979e6a220795553156d0e8363d9d37cd
yzh0120 <644193385@qq.com>
2022-04-09 13:56:29 +0800

    up

---
+++ java1.8_springboot_oa/src/test/java/com/yz/oa/OaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yz.oa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- java8_springboot_oa/src/main/java/com/yz/common/exception/IEnum.java
+++
@@ -1,7 +0,0 @@
-package com.yz.common.exception;
-
-public interface IEnum {
-    int getCode();
-
-    String getMsg();
-}
